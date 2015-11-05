d28cd1be4b43ace7d10321eec5bf00f72c1c27e7
Johannes Bertens <johannes.bertens@live.nl>
2015-11-05 19:22:36 +0100

    Updated the game!

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-// Call the console.log function.
-console.log("Hello World, from docker");
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
+
+var low = 0;
+var high = 100;
+var answer = Math.floor(Math.random() * (high - low) + low);
+
+console.log("Guess the number between " + low + " and " + high);
+rl.setPrompt('> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+	if (line.trim() < answer) console.log('Guess higher!');
+	if (line.trim() > answer) console.log('Guess lower!');
+	if (line.trim() == answer)
+	{
+		console.log('Correct!!');
+		rl.close();
+	}
+  rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
+});
+
